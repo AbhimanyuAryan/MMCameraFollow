9ea5c7d14987e28a3ce557d098cdd73c3d7453a5
DESKTOP-2LV9GDO\GK_Desk_07 <abhimanyu.chat@gmail.com>
2017-12-12 19:34:21 +0530

    Adding Tags to Network Objects

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -6,6 +6,34 @@ using UnityEngine.UI;
 public class SetupLocalPlayer : NetworkBehaviour {
 	public Text namePrefab;
 	public Text nameLabel;
+	public Transform namePos;
+	string textboxname = "";
+	[SyncVar (hook= "OnChangeName")] public string pName = "player";
+	
+	public void OnChangeName(string n)
+	{
+		// SyncVar part runs on the Client 
+		pName = n;
+		nameLabel.text = pName;
+	}
+
+	[Command] public void CmdChangeName(string newName)
+	{
+		// This part runs on server
+		pName = newName;
+		nameLabel.text = pName;
+	}
+
+	void OnGUI()
+	{
+		if(isLocalPlayer)
+		{
+			textboxname = GUI.TextField(new Rect(25, 15, 100, 25), textboxname);
+			if(GUI.Button(new Rect(130, 15, 35, 25), "SET"))
+				CmdChangeName(textboxname); 		
+		}
+	}
+
 	void Start () 
 	{
 		if(isLocalPlayer)
@@ -25,7 +53,7 @@ public class SetupLocalPlayer : NetworkBehaviour {
 
 	void Update()
 	{
-
+		Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+		nameLabel.transform.position = nameLabelPos;
 	}
-
 }
