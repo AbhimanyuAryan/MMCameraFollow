a41c245655c595a53201afde13a410f8e89edb57
DESKTOP-2LV9GDO\GK_Desk_07 <abhimanyu.chat@gmail.com>
2017-12-12 15:52:49 +0530

    You created project and adding label to game Object

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI; 
+
+public class SetupLocalPlayer : NetworkBehaviour {
+	public Text namePrefab;
+	public Text nameLabel;
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+			CameraFollow360.player = this.gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+
+		GameObject canvas = GameObject.FindWithTag("MainCanvas"); 
+		nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+		nameLabel.transform.SetParent(canvas.transform);
+	}
+
+	void Update()
+	{
+
+	}
+
+}
